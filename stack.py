1fd2c6f5d06b13a41ff9c5da33302fc7ab7c2fb9
Michael Koren <michael.koren@oracle.com>
2018-11-19 01:47:24 +0200

    Add regular stack with support for get current largest
    all methods are with max complexity of O(1)
    meaning push, pop, peek, and get_largest

---
+++ stack.py
@@ -0,0 +1,28 @@
+class Stack:
+
+    def __init__(self):
+        self._data = list()
+
+    def push(self, obj):
+        current_largest = max(obj, self.get_largest())
+        self._data.append((obj, current_largest))
+
+    def pop(self):
+        obj, _last_largest = self._data.pop(-1)
+        return obj
+
+    def _peek_head(self):
+        if self._data:
+            return self._data[-1]
+
+    def peek(self):
+        head = self._peek_head()
+        if head:
+            obj = head[0]
+            return obj
+
+    def get_largest(self):
+        head = self._peek_head()
+        if head:
+            current_largest = head[1]
+            return current_largest
