4461a036c334ecdf407ff06a465b413c6fe4facb
Michael Koren <michael.koren@oracle.com>
2018-11-19 01:52:08 +0200

    Add test_stack - py.test testing module for stack.py
    Add first test 'test_largest' with ordered list and random list
    
    every time an element from the list is pushed to the stack
    it is verified that the Stack.get_largest method
    returns the current maximum.

---
+++ test_stack.py
@@ -0,0 +1,18 @@
+import pytest
+from random import randrange
+from stack import Stack
+
+ORDERED_LIST = (1,2,3,4,5,6)
+RANDOM_LIST = tuple([randrange(-10,10) for each in range(6)])
+
+def _verify_largest(s):
+    largest_element = max(s._data, key=lambda data_pair:data_pair[0])[0] 
+    assert s.get_largest() == largest_element
+
+@pytest.mark.parametrize(
+    "lst", (ORDERED_LIST, RANDOM_LIST), ids=("ordered", "random"))
+def test_largest(lst):
+    s = Stack()
+    for element in lst:
+        s.push(element)
+        _verify_largest(s)
